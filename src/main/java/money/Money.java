c3ad9b0d4cca8e173154d2a241766c7977df4418
mateusquaresma <mateus.quaresma@protonmail.com>
2021-07-19 21:48:08 -0300

    Reached chapter 12

---
+++ src/main/java/money/Money.java
@@ -0,0 +1,48 @@
+package money;
+
+import java.util.Objects;
+
+public class Money {
+    public static Money dollar(int amount) {
+        return new Money(amount, "USD");
+    }
+
+    public static Money franc(int amount) {
+        return new Money(amount, "CHF");
+    }
+
+    protected int amount;
+
+    protected String currency;
+
+    protected Money(int amount, String currency) {
+        this.amount = amount;
+        this.currency = currency;
+    }
+
+    public Money times(int multiplier) {
+        return new Money(this.amount * multiplier, this.currency);
+    }
+
+    public String currency() {
+        return this.currency;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Money money = (Money) o;
+        return amount == money.amount && Objects.equals(currency, money.currency);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(amount);
+    }
+
+    @Override
+    public String toString() {
+        return amount + " " + currency;
+    }
+}
