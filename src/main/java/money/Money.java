deb2e0ab862cfefbe279c1fec4c29c0f7bf87ee3
mateusquaresma <mateus.quaresma@protonmail.com>
2021-07-20 21:05:22 -0300

    I reached chapter 14

--- src/main/java/money/Money.java
+++ src/main/java/money/Money.java
@@ -2,7 +2,7 @@ package money;
 
 import java.util.Objects;
 
-public class Money {
+public class Money implements Expression {
     public static Money dollar(int amount) {
         return new Money(amount, "USD");
     }
@@ -44,5 +44,13 @@ public class Money {
     @Override
     public String toString() {
         return amount + " " + currency;
+    }
+
+    public Expression plus(Money addend) {
+        return new Sum(this, addend);
+    }
+
+    public Money reduce(String to) {
+        return this;
     }
 }
