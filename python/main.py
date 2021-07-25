9da2fec67a9ae2e6d77d5dbb6a6782ffc8e46c7a
mateusquaresma <mateus.quaresma@protonmail.com>
2021-07-25 18:09:26 -0300

    Reached part 3 - Patterns for Test-Driven Development

--- python/main.py
+++ python/main.py
@@ -1,27 +1,109 @@
 class TestCase:
     def __init__(self, name):
         self.name = name
+        self.log = ""
 
-    def run(self):
-        method = getattr(self, self.name)
-        method()
+    def setup(self):
+        pass
+
+    def tear_down(self):
+        pass
+
+    def run(self, result):
+        result.test_started()
+        try:
+            self.setup()
+            method = getattr(self, self.name)
+            method()
+        except:
+            result.test_failed()
+        finally:
+            self.tear_down()
+
+
+class TestResult:
+    def __init__(self):
+        self.run_count = 0
+        self.error_count = 0
+
+    def test_started(self):
+        self.run_count = self.run_count + 1
+
+    def test_failed(self):
+        self.error_count = self.error_count + 1
+
+    def summary(self):
+        return "%d run, %d failed" % (self.run_count, self.error_count)
 
 
 class WasRun(TestCase):
     def __init__(self, name):
         TestCase.__init__(self, name)
-        self.wasRun = None
+
+    def setup(self):
+        self.log = "set_up "
 
     def test_method(self):
-        self.wasRun = True
+        self.log = self.log + "test_method "
+
+    def test_broken_method(self):
+        raise Exception
+
+    def tear_down(self):
+        self.log = self.log + "tear_down"
+
+
+class TestSuite:
+    def __init__(self):
+        self.tests = []
+
+    def add(self, test):
+        self.tests.append(test)
+
+    def run(self, result):
+        for test in self.tests:
+            test.run(result)
 
 
 class TestCaseTest(TestCase):
-    def test_running(self):
+    def setup(self):
+        self.test = WasRun("test_method")
+        self.result = TestResult()
+
+    def test_template_method(self):
         test = WasRun("test_method")
-        assert(not test.wasRun)
-        test.run()
-        assert(test.wasRun)
+        test.run(self.result)
+        assert ("set_up test_method tear_down" == test.log)
+
+    def test_result(self):
+        test = WasRun("test_method")
+        test.run(self.result)
+        assert ("1 run, 0 failed" == self.result.summary())
+
+    def test_failed_result(self):
+        test = WasRun("test_broken_method")
+        test.run(self.result)
+        assert ("1 run, 1 failed" == self.result.summary())
+
+    def test_failed_result_formatting(self):
+        result.test_started()
+        result.test_failed()
+        assert ("1 run, 1 failed" == self.result.summary())
+
+    def test_suite(self):
+        suite = TestSuite()
+        suite.add(WasRun("test_method"))
+        suite.add(WasRun("test_broken_method"))
+        suite.run(self.result)
+        assert("2 run, 1 failed" == self.result.summary())
 
 
-TestCaseTest("test_running").run()
+suite = TestSuite()
+suite.add(TestCaseTest("test_template_method"))
+suite.add(TestCaseTest("test_result"))
+suite.add(TestCaseTest("test_failed_result"))
+suite.add(TestCaseTest("test_failed_result_formatting"))
+suite.add(TestCaseTest("test_suite"))
+result = TestResult()
+suite.run(result)
+print(result.summary())
